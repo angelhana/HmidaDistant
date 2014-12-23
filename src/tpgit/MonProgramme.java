f3036267cbb02a8494ce12a55b8329dd9be506cd
HanaHmida <hana.hmida@telecom-paristech.fr>
2014-12-23 10:28:34 +0100

    modification des altitudes des deux avions dans le programme principal

--- src/tpgit/MonProgramme.java
+++ src/tpgit/MonProgramme.java
@@ -3,7 +3,14 @@ package tpgit;
 public class MonProgramme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Avion a1= new Avion();
+		Avion a2=new Avion();
+		
+		a1.setAltitude(1000);
+		a2.setAltitude(7000);
+		a1.setAltitude(2000);
+		a2.setAltitude(8000);
 
 	}
 
